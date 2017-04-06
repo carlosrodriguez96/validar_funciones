a09bdf46a10ada222f43e2aa516a60d22945ce8d
carlosrodriguez96 <esemismo96@gmail.com>
2017-04-06 18:40:34 -0500

    Create bibliotecas.php

---
+++ www/bibliotecas.php
@@ -0,0 +1,5 @@
+<link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+<script src="bootstrap/js/jquerymin.js"></script>
+<script src="bootstrap/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/angular.min.js"></script>
+<script type="text/javascript" src="js/mi_js.js"></script>
